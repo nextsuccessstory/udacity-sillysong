b541efa7186208964b99782d51c73ef692302d9e
Glenn-Arc Cachuela Pineda <glennpineda@hotmail.com>
2017-10-18 15:43:34 -0700

    Initial Commit

---
+++ Silly Song/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Silly Song
+//
+//  Created by Casa Gunnar on 10/18/17.
+//  Copyright © 2017 Next Success Story, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Silly Song/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Silly Song
+//
+//  Created by Casa Gunnar on 10/18/17.
+//  Copyright © 2017 Next Success Story, LLC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
